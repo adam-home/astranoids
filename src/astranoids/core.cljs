0ab15d746d3b88b16a170ff1dacb061bea2c815f
Adam Turnbull <adam@adamt.com>
2020-04-13 09:07:48 +0100

    Remove console reload message

--- src/astranoids/core.cljs
+++ src/astranoids/core.cljs
@@ -21,8 +21,6 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/astranoids/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
